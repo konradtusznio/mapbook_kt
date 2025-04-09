0a5812e900633d1279a65020b63e71aa222a7b1b
konradtusznio <konrad.tusznio@gmail.com>
2025-04-09 13:53:07 +0200

    wip: add new user function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import  users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 
 def main():
 
@@ -17,6 +17,8 @@ def main():
             break
         if choice == '1':
             get_user_info(users[1:])
+        if choice == '2':
+            add_user(users)
 
 if __name__ == '__main__':
     main()
